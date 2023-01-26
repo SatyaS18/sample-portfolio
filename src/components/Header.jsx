36aaa231e4840bd285def9b64db880c6468faff9
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-27 00:28:44 +0530

    added nav content to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <nav>
+        <NavContent />
+      </nav>
+    </>
+  );
 };
+
+const NavContent = () => (
+  <>
+    <h2>Satyakant Sahu</h2>
+    <div>
+      <a href="#home">Home</a>
+      <a href="#work">Work</a>
+      <a href="#timeline">Experiences</a>
+      <a href="#services">Services</a>
+      <a href="#testimonial">Testimonial</a>
+      <a href="#contact">Contact</a>
+    </div>
+    <a href="mailto:satyakantsahu18@gmail.com">
+      <button>Email</button>
+    </a>
+  </>
+);
 
 export default Header;
