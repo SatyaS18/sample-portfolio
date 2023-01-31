1de66cd4ff5b44b8a07aab8f186a156596e21c7a
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-31 15:25:48 +0530

    modified contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,40 @@
 import React from "react";
+import { useState } from "react";
 import vg from "../assets/vg.png";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const submitHandler = () => {};
   return (
     <div id="contact">
       <section>
-        <form action="">
+        <form action="" onSubmit={submitHandler}>
           <h2>Contact Me</h2>
 
-          <input type="text" placeholder="Your Name" required />
-          <input type="email" placeholder="Your Email" required />
-          <input type="text" placeholder="Your Message" required />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Your Name"
+            required
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Your Email"
+            required
+          />
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Your Message"
+            required
+          />
 
           <button type="submit">Send</button>
         </form>
